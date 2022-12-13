596b7f8c6bd3c13eabd213febc7e43f5c71ee6d0
KunihikoKido <kunihiko.kido@veltra.com>
2022-12-13 20:56:49 +0900

    Update

--- apps/classifications/test.py
+++ apps/classifications/test.py
@@ -5,12 +5,8 @@ from .models import Classification
 
 class TestCase(BaseTestCase):
     def setUp(self):
-        exists = Classification.exists_index()
-        if not exists:
-            Classification.create_index()
-            Classification.put_mapping()
+        Classification.create_index()
+        Classification.put_mapping()
 
     def tearDown(self):
-        exists = Classification.exists_index()
-        if exists:
-            Classification.delete_index()
+        Classification.delete_index()
--- apps/documents/test.py
+++ apps/documents/test.py
@@ -5,12 +5,8 @@ from .models import ProductDocument
 
 class TestCase(BaseTestCase):
     def setUp(self):
-        exists = ProductDocument.exists_index()
-        if not exists:
-            ProductDocument.create_index()
-            ProductDocument.put_mapping()
+        ProductDocument.create_index()
+        ProductDocument.put_mapping()
 
     def tearDown(self):
-        exists = ProductDocument.exists_index()
-        if exists:
-            ProductDocument.delete_index()
+        ProductDocument.delete_index()
